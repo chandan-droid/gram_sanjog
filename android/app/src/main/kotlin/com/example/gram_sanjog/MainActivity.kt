50b22046f4073bacaf130ffac4e694fc2870b000
dev <147903617+chandan-droid@users.noreply.github.com>
2025-06-08 23:29:39 +0530

    Initial setup completed with firebase integration

---
+++ android/app/src/main/kotlin/com/example/gram_sanjog/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gram_sanjog
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:gram_sanjog/view/home_page_view.dart';
+import 'firebase_options.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'News App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
